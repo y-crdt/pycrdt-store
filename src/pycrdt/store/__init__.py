2c69c16cef1bc913b6e5a524668f39a5f366f03a
Darshan808 <pranishpoudel10@gmail.com>
2025-05-02 00:05:02 +0545

    fix-import-error

--- src/pycrdt/store/__init__.py
+++ src/pycrdt/store/__init__.py
@@ -1,5 +1,6 @@
 from .store import BaseYStore as BaseYStore
 from .store import SQLiteYStore as SQLiteYStore
 from .store import TempFileYStore as TempFileYStore
+from .store import YDocNotFound as YDocNotFound
 
 __version__ = "0.1.0"
