c665b0edd2f63e3422ebfd3774bc84526b316330
Darshan808 <pranishpoudel10@gmail.com>
2025-05-18 15:23:51 +0545

    import-fileystore

--- src/pycrdt/store/__init__.py
+++ src/pycrdt/store/__init__.py
@@ -1,4 +1,5 @@
 from .store import BaseYStore as BaseYStore
+from .store import FileYStore as FileYStore
 from .store import SQLiteYStore as SQLiteYStore
 from .store import TempFileYStore as TempFileYStore
 from .store import YDocNotFound as YDocNotFound
